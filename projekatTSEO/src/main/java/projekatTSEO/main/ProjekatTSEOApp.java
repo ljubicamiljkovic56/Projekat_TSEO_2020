92b8c176742935aa3c6d86848b144e785860f5ac
ljubica_m56 <ljubica.m19@gmail.com>
2020-03-21 18:14:59 +0100

    Komit praznog projekta.

---
+++ projekatTSEO/src/main/java/projekatTSEO/main/ProjekatTSEOApp.java
@@ -0,0 +1,14 @@
+package projekatTSEO.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatTSEOApp{
+
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ProjekatTSEOApp.class, args);
+	}  
+
+}
