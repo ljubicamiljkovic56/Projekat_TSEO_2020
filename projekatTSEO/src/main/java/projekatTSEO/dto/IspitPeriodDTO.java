87a035095a994fc2ebe24e4133811727239b0d5e
HLEB <HLEB@DESKTOP-88A3L18>
2020-03-31 20:13:26 +0200

    period dto i servis

--- projekatTSEO/src/main/java/projekatTSEO/dto/IspitPeriodDTO.java
+++ projekatTSEO/src/main/java/projekatTSEO/dto/IspitPeriodDTO.java
@@ -1,5 +1,64 @@
 package projekatTSEO.dto;
 
-public class IspitPeriodDTO {
+import java.util.Date;
 
+import projekatTSEO.model.IspitPeriod;
+
+public class IspitPeriodDTO {
+	
+	private Long id;
+	private String ime;
+	private Date startDate;
+	private Date endDate;
+	
+	public IspitPeriodDTO() {
+		
+	}
+
+	public IspitPeriodDTO(IspitPeriod ispitPeriod) {
+		this(ispitPeriod.getId(), ispitPeriod.getIme(), ispitPeriod
+				.getStartDate(), ispitPeriod.getEndDate());
+	}
+	
+	public IspitPeriodDTO(Long id, String ime, Date startDate, Date endDate) {
+		super();
+		this.id = id;
+		this.ime = ime;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	
 }
--- projekatTSEO/src/main/java/projekatTSEO/service/IspitPeriodService.java
+++ projekatTSEO/src/main/java/projekatTSEO/service/IspitPeriodService.java
@@ -1,5 +1,39 @@
 package projekatTSEO.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import projekatTSEO.model.IspitPeriod;
+import projekatTSEO.repository.IspitPeriodRepository; 
+
+@Service
 public class IspitPeriodService {
+	@Autowired
+	IspitPeriodRepository ispitPeriodRepository;
+	
+	public IspitPeriod findOne(Long id) {
+		return ispitPeriodRepository.findById(id).orElse(null);
+	}
+
+	public List<IspitPeriod> findAll() {
+		return ispitPeriodRepository.findAll();
+	}
+	
+	public Page<IspitPeriod> findAll(Pageable page) {
+		return ispitPeriodRepository.findAll(page);
+	}
+
+	public IspitPeriod save(IspitPeriod ispitPeriod) {
+		return ispitPeriodRepository.save(ispitPeriod);
+	}
+
+	public void remove(Long id) {
+		ispitPeriodRepository.deleteById(id);
+	}
+	
 
 }
